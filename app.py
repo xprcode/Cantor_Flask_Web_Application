094da904fdb76909c105bf2745699aff2b0daafb
Piotr Kasperek <xprcode@onet.pl>
2024-02-05 09:54:03 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,20 +49,20 @@ class User(db.Model, UserMixin):
 
 
 class Portfolio(db.Model): 
-   id = db.Column(db.Integer, primary_key=True) 
-   currency_symbol = db.Column(db.String(3))
-   currency_name = db.Column(db.String(30))
-   currency_amount = db.Column(db.Integer)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True) 
+    currency_symbol = db.Column(db.String(3))
+    currency_name = db.Column(db.String(30))
+    currency_amount = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class History(db.Model): 
-   id = db.Column(db.Integer, primary_key=True) 
-   currency_symbol = db.Column(db.String(3))
-   currency_name = db.Column(db.String(30))
-   currency_amount = db.Column(db.Integer)
-   currency_price = db.Column(db.Integer)
-   date_of_action = db.Column(db.DateTime)
-   user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True) 
+    currency_symbol = db.Column(db.String(3))
+    currency_name = db.Column(db.String(30))
+    currency_amount = db.Column(db.Integer)
+    currency_price = db.Column(db.Integer)
+    date_of_action = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 @app.route('/init')
