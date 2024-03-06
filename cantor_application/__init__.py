0af2d782219c5b5847274d64d271075419a78843
Piotr Kasperek <xprcode@onet.pl>
2024-03-06 17:18:42 +0100

    Cleaning __init__ file

--- cantor_application/__init__.py
+++ cantor_application/__init__.py
@@ -17,25 +17,3 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = getenv('SQLALCHEMY_TRACK_MODIFICA
 
 db = SQLAlchemy(app)
 Migrate(app,db)
-
-
-
-# import os
-# from os import getenv
-# from dotenv import load_dotenv
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_migrate import Migrate
-
-# load_dotenv()
-
-# app = Flask(__name__)
-
-
-# app.config['SECRET_KEY'] = getenv('SECRET_KEY')
-# basedir = os.path.abspath(os.path.dirname(__file__))
-# app.config['SQLALCHEMY_DATABASE_URI'] = getenv('SQLALCHEMY_DATABASE_URI')
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
-
-# db = SQLAlchemy(app)
-# Migrate(app,db)
